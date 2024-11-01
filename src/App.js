2ca3b2fc5fa6b0a87a4e4cb1ff2c85542096ff4b
Wyatt <wnapier@bu.edu>
2024-11-01 15:11:44 -0400

    fixing typo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
 
   useEffect(() => {
-    fetch('/api/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
+    fetch("/api/time")
+      .then((res) => res.json())
+      .then((data) => {
+        setCurrentTime(data.time);
+      });
   }, []);
 
-function App() {
   return (
     <div className="App">
       <header className="App-header">
